e3e408ee4fc6e18317a0578f7757a71ba7a0b615
Borja <mrborjabtf@gmail.com>
2019-11-09 19:58:53 +0100

    Pathfinding acabat + creacio prefab tropa

--- Assets/Projecte/Scripts/TroopController.cs
+++ Assets/Projecte/Scripts/TroopController.cs
@@ -1,19 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
 public class TroopController : MonoBehaviour
 {
-    public Camera cam;
-    
+    public Troop troop;
+    public NavMeshAgent agent;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            Raycast hit;
-            Physics.Raycast(ray, out hit);
-        }
+        agent.SetDestination(troop.DetectClosestEnemy().transform.position);
     }
 }
