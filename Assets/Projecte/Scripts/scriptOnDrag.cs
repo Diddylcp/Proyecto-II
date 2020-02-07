059030cd15d4573d78f8ac3cfd1b94ecf7198c91
Borja <borja.tapia@enti.cat>
2020-02-07 13:28:57 +0100

    Escenas borradas, cambio script Drag

--- Assets/Projecte/Scripts/scriptOnDrag.cs
+++ Assets/Projecte/Scripts/scriptOnDrag.cs
@@ -1,26 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class ScriptOnDrag : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    [SerializeField] private Canvas canvas;
+    [SerializeField] private GameObject soldierPrefab;
     private RectTransform rectTransform;
     GameObject player;
     PlayerController playerController;
     [SerializeField] private GameObject soldierImage;
+    private GameObject soldierImageInstanciated;
     [SerializeField] private int soldierCost;
-
-    private void Awake()
-    {
-        rectTransform = GetComponent<RectTransform>();
-    }
+    private Vector2 soldierPos = Vector2.zero;
+    Collider2D col;
 
     // Start is called before the first frame update
     public void AddImageOnHUD()
     {
-        if (tag == "AllyTroop") player = GameObject.Find("PlayerEconomy");
+        if (tag == "AllyTroop") player = GameObject.Find("AllyEconomy");
         else player = GameObject.Find("EnemyEconomy");
         playerController = player.GetComponent<PlayerController>();
         if (playerController.GetMoney() > soldierCost)
@@ -34,28 +33,30 @@ public class ScriptOnDrag : MonoBehaviour, IPointerDownHandler, IBeginDragHandle
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
-        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        soldierPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        soldierImageInstanciated.transform.position = soldierPos;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("OnEndDrag");
+        Destroy(soldierImageInstanciated);
+        col = Physics2D.OverlapCircle(Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.1f);
+        // La colision no tienen ningun tag
+        Debug.Log(col.gameObject.tag);
+        if (col.tag == "Respawn")
+        {
+            Instantiate(soldierPrefab, soldierPos, Quaternion.identity);
+        }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        Vector2 soldierPos;
-        if (Physics.Raycast(ray, out hit, 1000))
+        col = Physics2D.OverlapCircle(Camera.main.ScreenToWorldPoint(Input.mousePosition), 0.1f);
+        if (col != null)
         {
-            soldierPos = (Vector2)hit.point;
-            Instantiate(soldierImage, new Vector3(soldierPos.x, soldierPos.y, 0), Quaternion.identity);
+            soldierPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            soldierImageInstanciated = Instantiate(soldierImage, soldierPos, Quaternion.identity);
         }
-        //if (playerController.GetMoney() > soldierCost)
-        //Instantiate(soldierImage, this.transform);
-        Debug.Log("OnPointerDown");
-        
+        rectTransform = soldierImage.GetComponent<RectTransform>();
     }
 }
