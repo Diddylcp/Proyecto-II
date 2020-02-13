2f0c35c809fea1b864c6a4d3b5970ddf26402a91
David <david.martin@enti.cat>
2020-02-13 11:37:55 +0100

    Script MyNode

---
+++ Assets/Projecte/Scripts/RealPathfinder/MyNode.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNode : MonoBehaviour
+{
+    public Vector2 pos;
+    public List<MyNode> neighbours;
+    public int gCost;
+    public int hCost;
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+
+    public int CompareTo(MyNode nodeToCompare)
+    {
+        int compare = fCost.CompareTo(nodeToCompare.fCost);
+        if (compare == 0)
+        {
+            compare = hCost.CompareTo(nodeToCompare.hCost);
+        }
+        return -compare;
+    }
+}
