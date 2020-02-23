e67b3e0540dd56df2d8bc45a78b926a48546c11d
Borja <mrborjabtf@gmail.com>
2020-02-23 18:19:41 +0100

    Pause Menu

---
+++ Assets/Projecte/Scripts/Troops/TowerScript.cs
@@ -0,0 +1,252 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum TowerType { NORMAL, MAGE_TOWER, ARCHER_TOWER, WARRIOR_TOWER, GOLD_TOWER, SPEED_TOWER, COUNT };
+
+
+public struct TowerStates
+{
+    public float health;
+    public float startHealth;
+    public float area;
+    public int damage;
+    public float range;
+    public float attackSpeed;
+    public int moneyPerSecond;
+
+    public void SetStats(TowerType tipo)
+    {
+        switch (tipo)
+        {
+            case TowerType.NORMAL:
+                health = 1000;       // Vida original 1500
+                damage = 130;
+                attackSpeed = 0.8f;
+                range = 1f; //11
+                moneyPerSecond = 10;
+                area = 0;
+                break;
+            case TowerType.MAGE_TOWER:
+                health = 1800;
+                damage = 40;
+                attackSpeed = 1.6f;
+                range = 4;
+                moneyPerSecond = 10;
+                area = 2;
+                break;
+            case TowerType.ARCHER_TOWER:
+                health = 1800;
+                damage = 30;
+                attackSpeed = 2.1f;
+                range = 5;
+                moneyPerSecond = 10;
+                area = 0;
+                break;
+
+            case TowerType.GOLD_TOWER:
+                health = 1800;
+                damage = 30;
+                attackSpeed = 1.6f;
+                range = 4;
+                moneyPerSecond = 15;
+                area = 0;
+                break;
+
+            case TowerType.SPEED_TOWER:
+                health = 1800;
+                damage = 30;
+                attackSpeed = 2f;
+                range = 4;
+                moneyPerSecond = 10;
+                area = 0;
+                break;
+
+            case TowerType.WARRIOR_TOWER:
+                health = 2000;
+                damage = 30;
+                attackSpeed = 1.6f;
+                range = 4;
+                moneyPerSecond = 10;
+                area = 0;
+                break;
+        }
+        startHealth = health;
+    }
+}
+
+public class TowerScript : MonoBehaviour
+{
+    public PlayerController player;// jugador el que controla
+    [SerializeField] PlayerController allyPlayer, enemyPlayer;
+    public TowerStates stats;
+    private float speed;
+    public TowerType type;
+    GameObject objective;  //Al que atacara
+    Vector2 pos;
+    Vector2 posMouse;
+    public GameObject respawnArea;
+    bool isClicked = false;
+    public MilloresTower enhanceButtons;
+    MilloresTower hudEnhance;
+    Vector3 positionToShowEnhance;
+    public GameObject selected;
+    public Image HealthBar;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionToShowEnhance = new Vector3(454.3f, 36.9375f, 0);
+        type = TowerType.NORMAL;
+        objective = AnyoneToAttack();
+        if (tag == "AllyTower") player = allyPlayer;
+        else player = enemyPlayer;
+        stats.SetStats(type);
+        StartCoroutine(AttackEnemy());
+        StartCoroutine(WaitSec());
+    }
+
+    [SerializeField] private Material MaterialEnemigo;
+    [SerializeField] private Material MaterialAliado;
+    [SerializeField] private string team;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(objective == null)
+        {
+            objective = AnyoneToAttack();
+        }
+        else
+        {
+            if (!StillInRange())
+            {
+                objective = AnyoneToAttack();
+            }
+        }
+
+
+    }
+
+    //Si segueix en rang enemic
+    bool StillInRange()
+    {
+        float distance;
+        distance = Vector3.Distance(pos, objective.transform.position);
+        return (Mathf.Abs(distance) < stats.range);
+    }
+
+    //Augmenta el money
+    void DropCoin()
+    {
+        player.SumMoney(stats.moneyPerSecond);
+
+    }
+
+    IEnumerator WaitSec()
+    {
+        // wait for 1 second
+        yield return new WaitForSeconds(1.0f);
+        DropCoin();
+        StartCoroutine(WaitSec());
+    }
+    //Al clickar
+    public void SetIsClicked(bool a)
+    {
+        isClicked = a;
+    }
+
+    private void OnMouseDown()
+    {
+        if (tag == "AllyTower")
+        { 
+            if (!player.GetPlayerWithTower())
+            {
+                player.SetPlayerWithTower(true);
+                hudEnhance = Instantiate(enhanceButtons, positionToShowEnhance, Quaternion.identity, GameObject.Find("ButtonsHUD").transform);
+                hudEnhance.tower = this;
+                hudEnhance.tower.selected.GetComponent<SpriteRenderer>().enabled = true;
+            }
+            else
+            {
+                hudEnhance = GameObject.FindObjectOfType<MilloresTower>();
+                hudEnhance.tower.selected.GetComponent<SpriteRenderer>().enabled = false;
+                hudEnhance.tower = this;
+                hudEnhance.tower.selected.GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
+        
+    }
+    private GameObject AnyoneToAttack()
+    {
+        GameObject[] gos;
+        if (tag == "AllyTower") gos = GameObject.FindGameObjectsWithTag("EnemyTroop");
+        else gos = GameObject.FindGameObjectsWithTag("AllyTroop");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        pos = transform.position;
+        foreach (GameObject go in gos)
+        {
+            float curDistance = Vector3.Distance(pos, go.transform.position);
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
+    IEnumerator AttackEnemy()
+    {
+        if(objective != null)
+        {
+            if (StillInRange())
+            {
+                if ((objective.tag == "AllyTroop" && this.tag == "EnemyTower") || (objective.tag == "EnemyTroop" && this.tag == "AllyTower"))
+                {
+                    objective.GetComponent<Troop>().TakeDamage(stats.damage);
+                }
+            }                
+        }
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(AttackEnemy());
+    }
+
+    public void TakeDamage(int damage)
+    {
+        stats.health -= damage;
+        HealthBar.fillAmount = stats.health / stats.startHealth;
+        if (stats.health <= 0)
+        {
+            ChangeTeam();
+        }
+    }
+
+    public void ChangeTeam()
+    {
+        if(team == "AllyTower")
+        {
+            this.tag = "EnemyTower";
+            this.GetComponent<MeshRenderer>().material = MaterialEnemigo;
+            stats.health = 1500;
+            HealthBar.fillAmount = stats.health / stats.startHealth;
+            player = enemyPlayer;
+            respawnArea.tag = "EnemyRespawn";
+            
+        }
+        else if(team == "EnemyTower")
+        {
+            this.tag = "AllyTower";
+            this.GetComponent<MeshRenderer>().material = MaterialAliado;
+            stats.health = 1500;
+            HealthBar.fillAmount = stats.health / stats.startHealth;
+            player = allyPlayer;
+            respawnArea.tag = "Respawn";
+        }
+        team = this.tag;
+        
+    }
+}
---
+++ Assets/Projecte/Scripts/Troops/Troop.cs
@@ -0,0 +1,299 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class Troop : MonoBehaviour
+{
+    protected enum TroopState { INIT, MOVING, ATTACKING, DYING, COUNT};
+    public struct ability
+    {
+        [SerializeField]public float health;
+        public float movSpeed;
+        public float area;
+        public int residualDamage;
+        public int damage;
+        public float range;
+        public float attackSpeed;
+    };
+    protected Animator myAnimator;
+    [SerializeField]protected TroopState troopState;
+    [SerializeField]protected float startHealth;
+    public string team;
+    public ability stats;
+    public GameObject troopObjective;
+    protected MyNode towerToMove;
+    [SerializeField] private Image healthBar;
+    public GameObject projectile;
+    protected GraphPathfinder pathRequest;
+    protected MyNode currNode;
+
+    protected bool isAttacking = false, isMoving = false;
+    protected int targetIndex = 0;
+
+    
+    protected void Start()
+    {
+        myAnimator = GetComponentInChildren<Animator>();
+        troopState = TroopState.INIT;
+        team = tag;
+        troopObjective = DetectClosestEnemy();
+        currNode = findClosestNode();
+        pathRequest = new GraphPathfinder();
+    }
+
+    void Update()
+    {
+        switch (troopState)
+        {
+            case TroopState.INIT:
+                if (pathRequest.findPath(currNode, towerToMove) && !StillInRange(troopObjective))
+                {
+                    troopState = TroopState.MOVING;
+                    myAnimator.SetBool("Running", true);
+                    myAnimator.SetBool("Attack", false);
+                }
+                else if (troopObjective != null)
+                    if (StillInRange(troopObjective))
+                    {
+                        troopState = TroopState.ATTACKING;
+                        myAnimator.SetBool("Running", false);
+                        myAnimator.SetBool("Attack", true);
+                        StartCoroutine(Attack());
+                    }
+                break;
+
+            case TroopState.MOVING:
+                if (!AmIAlive())
+                {
+                    isMoving = false;
+                    troopState = TroopState.DYING;
+                }
+                else if (StillInRange(troopObjective))
+                {
+                    isMoving = false;
+                    troopState = TroopState.ATTACKING;
+                    myAnimator.SetBool("Attack", true);
+                    myAnimator.SetBool("Running", false);
+                    StartCoroutine(Attack());
+                }
+                else
+                {
+                    FollowPath();
+                    isMoving = true;
+                }
+                break;
+
+            case TroopState.ATTACKING:
+                if (!AmIAlive())
+                {
+                    isAttacking = false;
+                    StopCoroutine(Attack());
+                    troopState = TroopState.DYING;
+                }
+                else if ((troopObjective == null || !StillInRange(troopObjective)) && pathRequest.findPath(currNode, towerToMove))
+                {
+                    targetIndex = 0;
+                    StopCoroutine(Attack());
+                    isAttacking = false;
+                    myAnimator.SetBool("Running", true);
+                    myAnimator.SetBool("Attack", false);
+                    troopState = TroopState.MOVING;
+                }
+                break;
+
+            case TroopState.DYING:
+                StopAllCoroutines();
+                Destroy(this.gameObject);
+                break;
+        }
+        currNode = findClosestNode();
+    }
+
+    public MyNode findClosestNode()
+    {
+        GameObject[] gosNodes;
+        gosNodes = GameObject.FindGameObjectsWithTag("Node");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+
+        foreach(GameObject go in gosNodes)
+        {
+            float currDistance = Vector2.Distance(this.transform.position, go.transform.position);
+            if(currDistance < distance)
+            {
+                closest = go;
+                distance = currDistance;
+            }
+        }
+        return closest.GetComponent<MyNode>();
+    }
+
+    public GameObject DetectClosestEnemy()
+    {
+        GameObject[] gosTroops;
+        if (tag == "AllyTroop")
+        {
+            gosTroops = GameObject.FindGameObjectsWithTag("EnemyTroop");    
+        }
+        else
+        {
+            gosTroops = GameObject.FindGameObjectsWithTag("AllyTroop");
+        }
+        GameObject closest = DetectClosestTower();
+        float distance = Vector3.Distance(this.transform.position, closest.transform.position);
+        foreach (GameObject go in gosTroops)
+        {
+            float curDistance = Vector3.Distance(this.transform.position, go.transform.position);
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
+    }
+
+    public GameObject DetectClosestTower()
+    {
+        GameObject[] gosTower;
+        if (tag == "AllyTroop")
+        {
+            gosTower = GameObject.FindGameObjectsWithTag("EnemyTower");
+        }
+        else
+        {
+            gosTower = GameObject.FindGameObjectsWithTag("AllyTower");
+        }
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        foreach (GameObject go in gosTower)
+        {
+            float curDistance = Vector3.Distance(this.transform.position, go.transform.position);
+            if (curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        towerToMove = closest.GetComponentInChildren<MyNode>();
+        return closest;
+    }
+
+    protected bool StillInRange(GameObject objective)     // Checks if troop is still in range of the enemy
+    {
+        float distance;
+        if (objective == null) return false;
+        distance = Vector2.Distance(this.transform.position, objective.transform.position);
+        return (distance < stats.range);
+    }
+
+    protected void AttackEnemy(GameObject enemy)          // Attacks the enemy
+    {
+        enemy.GetComponent<Troop>().TakeDamage(stats.damage);
+    }
+
+    protected void AttackTower(GameObject tower)          // Attacks the tower
+    {
+        tower.GetComponent<TowerScript>().TakeDamage(stats.damage);
+    }
+
+    protected bool AmIAlive()                             // Checks if he is alive
+    {
+        if(stats.health <= 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    protected void CapturingTower(GameObject _tower)      // Captures the tower
+    {
+
+    }
+
+    public void TakeDamage(int _damage)
+    {
+        stats.health -= _damage;
+        healthBar.fillAmount = stats.health / startHealth;
+    }
+
+    public Quaternion vectorRotation(Vector3 thisPos, Vector3 targetPos)
+    {
+        return Quaternion.LookRotation(targetPos) * Quaternion.Inverse(Quaternion.LookRotation(thisPos));
+    }
+
+    protected IEnumerator Attack()
+    {
+        if (StillInRange(troopObjective))
+        {
+            if (troopObjective.GetComponent<Troop>() != null)
+            {
+                AttackEnemy(troopObjective);
+                ShootProjectile();
+            }
+            else if (troopObjective.GetComponent<TowerScript>() != null)
+            {
+                AttackTower(troopObjective);
+                ShootProjectile();
+                if ((this.tag == "AllyTroop" && troopObjective.GetComponent<TowerScript>().tag == "AllyTower") || (this.tag == "EnemyTroop" && troopObjective.GetComponent<TowerScript>().tag == "EnemyTower"))
+                {
+                    troopObjective = DetectClosestEnemy();
+                }
+            } 
+        }
+        yield return new WaitForSeconds(this.stats.attackSpeed);
+        StartCoroutine(Attack());
+    }
+
+    protected void FollowPath()
+    {
+        if (targetIndex < pathRequest.waypoints.Length)
+        {
+            Vector2 currWaypoint = pathRequest.waypoints[targetIndex];
+            if (towerToMove != null)
+            {
+                if (!StillInRange(troopObjective))
+                {
+                    if (Vector2.Distance(transform.position, currWaypoint) < 0.1f*Time.deltaTime)
+                    {
+                        targetIndex++;
+                    }
+                    transform.position = Vector2.MoveTowards(transform.position, currWaypoint, stats.movSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+
+    protected void ShootProjectile()
+    {
+        Vector3 vectorToEnemy = troopObjective.transform.position - this.transform.position;
+        GameObject projectileSpawned = Instantiate(projectile, this.transform.position, Quaternion.LookRotation(vectorToEnemy)) as GameObject;
+    }
+
+    /*public void OnDrawGizmos()
+    {
+        if (pathRequest != null)
+        {
+            for (int i = targetIndex; i < pathRequest.waypoints.Length; i++)
+            {
+                Gizmos.color = Color.black;
+                Gizmos.DrawCube(pathRequest.waypoints[i], new Vector2(0.12f, 0.12f));
+
+                if (i == targetIndex)
+                {
+                    Gizmos.DrawLine((Vector2)transform.position, pathRequest.waypoints[i]);
+                }
+                else
+                {
+                    Gizmos.DrawLine(pathRequest.waypoints[i - 1], pathRequest.waypoints[i]);
+                }
+            }
+        }
+    }*/
+}
