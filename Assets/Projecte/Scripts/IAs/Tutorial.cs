ca4ed561f063bbb3b66f65240071400c80651bf1
Borja <mrborjabtf@gmail.com>
2020-02-25 00:11:17 +0100

    Tutorial Fixed

--- Assets/Projecte/Scripts/IAs/Tutorial.cs
+++ Assets/Projecte/Scripts/IAs/Tutorial.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
-    bool firstText = false, secondText = false;
-    [SerializeField] GameObject buttonsHud;
-    // Update is called once per frame
+    bool firstText = false, secondText = false, thirdText = false, firstUnit = false;
+    [SerializeField] GameObject buttonsHud, areaSpawn, mageButton, archerButton, otherButtons;
+
     void Start()
     {
         Time.timeScale = 0;
@@ -21,8 +21,35 @@ public class Tutorial : MonoBehaviour
                 firstText = true;
                 GameObject.Find("TutorialCanvas").SetActive(false);
                 buttonsHud.SetActive(true);
+                areaSpawn.SetActive(true);
+            }
+            else if (!secondText)
+            {
+                secondText = true;
                 Time.timeScale = 1;
-            } 
+                areaSpawn.SetActive(false);
+            }
+            else if (!thirdText)
+            {
+                buttonsHud.SetActive(true);
+                otherButtons.SetActive(false);
+                Time.timeScale = 1;
+            }
         }
+        if(GameObject.Find("AllyTroopWarrior Variant(Clone)") != null && !firstUnit)
+        {
+            StartCoroutine("ShowMageButton");
+            firstUnit = true;
+        }
+    }
+
+    IEnumerator ShowMageButton()
+    {
+        yield return new WaitForSeconds(2.5f);
+        Time.timeScale = 0;
+        otherButtons.SetActive(true);
+        mageButton.SetActive(true);
+        archerButton.SetActive(true);
+        buttonsHud.SetActive(false);
     }
 }
