7b095aaef97b638c5dd749272df594ae76711f3f
Oriol Comas <oriol799@gmail.com>
2020-03-27 14:07:23 +0100

    barra vida es fica frontal a la càmera

--- Assets/CanvasControl.cs
+++ Assets/CanvasControl.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class CanvasControl : MonoBehaviour
 {
+    float rotX;
+
+    void Start()
+    {
+        
+        rotX = Camera.main.transform.eulerAngles.x * (-1);
+        transform.Rotate(rotX, 0, 0);
+    }
     // Update is called once per frame
     void Update()
     {
-       transform.LookAt(Camera.main.transform.position, Camera.main.transform.up);
+        // transform.LookAt(Camera.main.transform.position, Camera.main.transform.up);
+       // transform.Rotate(rotX, 0, 0);
     }
 }
