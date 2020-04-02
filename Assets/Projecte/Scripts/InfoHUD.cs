8644368b8d06d53205af04ff6fa6fb19511b2ef1
Oriol Comas <oriol799@gmail.com>
2020-04-02 20:21:20 +0200

    InfoHud
    Descripcións de les millores editades

--- Assets/Projecte/Scripts/InfoHUD.cs
+++ Assets/Projecte/Scripts/InfoHUD.cs
@@ -25,32 +25,32 @@ public class InfoHUD : MonoBehaviour
     {
         gameObject.GetComponent<CanvasRenderer>().SetAlpha(1f);
         text.GetComponent<CanvasRenderer>().SetAlpha(1f);
-        text.text = "Magic Tower \n\n +300 health \n +10 damage \n +05 money per second \n Your magic soldiers will make \n damage in area \n ";
+        text.text = "Magic Tower \n\n +300 health \n +10 damage \n +5 money per second \n Your magic soldiers will make \n damage in area \n ";
     }
 
     public void OnHoverWarriorEnhancement()
     {
         gameObject.GetComponent<CanvasRenderer>().SetAlpha(1f);
         text.GetComponent<CanvasRenderer>().SetAlpha(1f);
-        text.text = "Warrior Tower \n\n +500 health \n +05 money per second \n Your warrior soldiers will return \n 25% of the damage taken \n";
+        text.text = "Warrior Tower \n\n +500 health \n +5 money per second \n Your warrior soldiers will return \n 25% of the damage taken \n";
     }
     public void OnHoverArcherEnhancement()
     {
         gameObject.GetComponent<CanvasRenderer>().SetAlpha(1f);
         text.GetComponent<CanvasRenderer>().SetAlpha(1f);
-        text.text = "Archer Tower \n\n +300 health \n +0.5 dps \n +4 Attack Range \n +05 money per second \n";
+        text.text = "Archer Tower \n\n +300 health \n +0.5 dps \n +4 Attack Range \n +5 money per second \n";
     }
     public void OnHoverVelocityEnhancement()
     {
         gameObject.GetComponent<CanvasRenderer>().SetAlpha(1f);
         text.GetComponent<CanvasRenderer>().SetAlpha(1f);
-        text.text = "Velocity Tower \n\n +300 health \n +0.5 dps  \n +05 money per second \n +10% of velocity on troops.\n";
+        text.text = "Velocity Tower \n\n +300 health \n +0.5 dps  \n +5 money per second \n +10% of velocity on troops.\n";
     }
     public void OnHoverEconomyEnhancement()
     {
         gameObject.GetComponent<CanvasRenderer>().SetAlpha(1f);
         text.GetComponent<CanvasRenderer>().SetAlpha(1f);
-        text.text = "Economy Tower \n\n +300 health \n +5 coins per second \n";
+        text.text = "Economy Tower \n\n +300 health \n +20 coins per second \n";
     }
     public void OnLeftMouse()
     {
