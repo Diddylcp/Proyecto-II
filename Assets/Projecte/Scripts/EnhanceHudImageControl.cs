9a1781f50dddc2cb1cfc72c1c13d11eec7967e93
Oriol Comas <oriol799@gmail.com>
2020-05-24 01:01:42 +0200

    FeedbackButtons de mejora de torres

---
+++ Assets/Projecte/Scripts/EnhanceHudImageControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EnhanceHudImageControl : MonoBehaviour
+{
+    [SerializeField] private Sprite buttonHoverImage;
+    [SerializeField] private Sprite buttonNormalImage;
+
+    private Image imageEnhance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        imageEnhance = this.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+   public void ImageToHover()
+    {
+        imageEnhance.sprite = buttonHoverImage;
+    }
+    public void ImageToNormal()
+    {
+        imageEnhance.sprite = buttonNormalImage;
+    }
+}
