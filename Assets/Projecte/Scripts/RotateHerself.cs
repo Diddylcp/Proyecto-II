d726858bb87b9861914dec3bd3d0f191829fb744
Borja <mrborjabtf@gmail.com>
2020-06-03 06:22:05 +0200

    New Texture Beam

---
+++ Assets/Projecte/Scripts/RotateHerself.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateHerself : MonoBehaviour
+{
+    private float rotation = 0;
+    void Update()
+    {
+        rotation -= Time.deltaTime * 30;
+        this.transform.rotation = Quaternion.Euler(0, rotation, rotation);
+    }
+}
---
+++ Assets/Projecte/Scripts/ScrollTexture.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+    [SerializeField] float scrollX = 0.5f;
+    [SerializeField] float scrollY = 0.5f;
+
+    float offsetX = 0;
+
+    void Update()
+    {
+        offsetX += Time.deltaTime * 10;
+        GetComponent<LineRenderer>().material.mainTextureOffset = new Vector2(offsetX, 0);
+    }
+}
